a4052e719d21f7037cf4a92e640ad6bb5c918fc4
niluwka0403 <ninimamatova0403@gmail.com>
2024-02-03 20:44:47 +0500

    Nested Loops

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -3,5 +3,18 @@ package school.mjc.stage0.loops.task5;
 public class Cross {
     public void printCross(int sideLength) {
 
+        for (int i = 1; i <= sideLength; i++) {
+            for (int j = 1; j <= sideLength; j++) {
+                if (i == (sideLength/2 + 1) )
+                    System.out.print('8');
+                else {
+                    if (j == (sideLength/2 + 1) )
+                        System.out.print('8');
+                    else
+                        System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height; j++) {
+                if (i >= j && i >= (height + 1) - j || (i <= j && i <= (height + 1) - j)){
+                    System.out.print('8');}
+                else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
     }
 }
